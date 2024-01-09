362f2122104828544d56da024b90eda98e2f3433
Bartosz Ciesielczyk <98945375+bciesielczyk@users.noreply.github.com>
2024-01-09 13:55:08 +0100

    008_print_numbers - JS & RUST

---
+++ js/008_print_numbers/index.js
@@ -0,0 +1,6 @@
+// --- Directions
+// Write a loop, that prints all three-digits numbers in decreasing order (999, 998, ..., 101, 100)
+
+for(let i = 999; i >= 100; i--) {
+    console.log(i)
+}
---
+++ rst/src/print_numbers.rs
@@ -0,0 +1,5 @@
+pub fn print_numbers() {
+    for i in (100..=999).rev() {
+        println!("{}", i);
+    }
+}
